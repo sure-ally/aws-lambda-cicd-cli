b32fbdcd1fb7b83e1c0a02828c2205625ba10d91
skyd-at-359524518288 <ysurendrak@gmail.com>
2021-07-22 15:57:16 -0400

    csv-read: added 5

--- csv-read.py
+++ csv-read.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
     df = pd.DataFrame()
     print(df)
 
-    list = ['one', 'two', 'three', 'four']
+    list = ['one', 'two', 'three', 'four', 'five']
 
     df = pd.DataFrame(list)
     print(df)
